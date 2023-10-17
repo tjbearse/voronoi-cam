ffc1f68b603dab5a379cb5868edb7e20c7dff27b
TJ Bearse <5686806+tjbearse@users.noreply.github.com>
2023-10-16 19:56:39 -0700

    commit WIP using git-management

---
+++ Assets/script/FrameRate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Rebound {
+	public class FrameRate : MonoBehaviour
+	{
+		[SerializeField]
+		[Range(10, 60)]
+		int frameRate = 30;
+		void Update()
+		{
+			Application.targetFrameRate = frameRate;
+		}
+	}
+
+}
--- Assets/script/VoronoiCamera.cs
+++ Assets/script/VoronoiCamera.cs
@@ -19,6 +19,7 @@ public class VoronoiCamera : MonoBehaviour {
 
 	public Vector2 splitOffsetS = Vector2.zero; // screen space offset
 	public bool rotateOffset = true;
+	public int superElipseN = 2;
 
     void Start() {
 		float z = -Cam1.transform.position.z; // inflexible
@@ -54,7 +55,11 @@ public class VoronoiCamera : MonoBehaviour {
 		CamTexture.material.SetFloat("_SplitDist", splitness);
 		CamTexture.material.SetVector("_Origin", rotSplit);
 
-		baseOffset = Vector2.Scale(diff.normalized, camExtent/2);
+		// formerly
+		// baseOffset = Vector2.Scale(diff.normalized, camExtent/2);
+		// TODO this is the right direction but needs to be rounded off and more continuous
+		// what we really want is a super elipse https://en.wikipedia.org/wiki/Superellipse
+		baseOffset = projectToSuperElipse(diff.normalized, camExtent, superElipseN) / 2;
 		if (rotateOffset) {
 			splitOffsetWorld = Vector2.Scale(camExtent, rot * splitOffsetS);
 		} else {
@@ -62,17 +67,67 @@ public class VoronoiCamera : MonoBehaviour {
 		}
 		alignedOffset = baseOffset.normalized * splitOffsetWorld.magnitude * Mathf.Cos(Mathf.Deg2Rad * Vector3.Angle(baseOffset, splitOffsetWorld));
 
-		Vector3 splitCamPos = baseOffset - alignedOffset;
-		Vector3 offset = Vector3.Lerp(diff/2, splitCamPos, splitness);
-		Vector3 pos = Player1.transform.position + offset;
+		Vector3 unsplitPosition = getUnsplitPosition(Player1.transform.position, Player2.transform.position, Cam1.transform.position, margin*camExtent);
+
+
+		Vector3 splitCamPos = Player1.transform.position + baseOffset - alignedOffset;
+		Vector3 pos = Vector3.Lerp(unsplitPosition, splitCamPos, splitness);
 		pos.z = Cam1.transform.position.z;
 		Cam1.transform.position = pos;
 
-		splitCamPos = -baseOffset - alignedOffset;
-		offset = Vector2.Lerp(-diff/2, splitCamPos, splitness);
-		pos = Player2.transform.position + offset;
+		splitCamPos = Player2.transform.position -baseOffset - alignedOffset;
+		pos = Vector3.Lerp(unsplitPosition, splitCamPos, splitness);
 		pos.z = Cam2.transform.position.z;
 		Cam2.transform.position = pos;
+	}
+
+	Vector3 getUnsplitPosition(Vector3 pos1, Vector3 pos2, Vector3 curr, Vector3 extent) {
+		// return Player1.transform.position  + diff/2;
+		Vector3 center = (pos2 - pos1)/ 2 + pos1;
+		float xLb = Mathf.Min(Mathf.Max(pos1.x, pos2.x) - extent.x, center.x + extent.x);
+		float xUb = Mathf.Max(Mathf.Min(pos1.x, pos2.x) + extent.x, center.x - extent.x);
+		curr.x = Mathf.Clamp(curr.x, xLb, xUb);
+		float yLb = Mathf.Min(Mathf.Max(pos1.y, pos2.y) - extent.y, center.y + extent.y);
+		float yUb = Mathf.Max(Mathf.Min(pos1.y, pos2.y) + extent.y, center.y - extent.y);
+		curr.y = Mathf.Clamp(curr.y, yLb, yUb);
+		return curr;
+	}
+
+	Vector2 projectToSuperElipse(Vector2 norm, Vector2 extents, int power) {
+		// y/x = (b/a)(tan t) ^ (2/n)
+		float t;
+		if (Mathf.Approximately(norm.x, 0)) {
+			t = Mathf.PI / 2;
+		} else if (Mathf.Approximately(norm.y, 0)) {
+			t = 0;
+		} else {
+			t = Mathf.Atan(Mathf.Pow((extents.x / extents.y) * Mathf.Abs(norm.y / norm.x), power/2f));
+			// = Mathf.Atan2(norm.y, norm.x);
+		}
+		float cos_t = Mathf.Cos(t);
+		float sin_t = Mathf.Sin(t);
+		return new Vector2(
+			Mathf.Pow(cos_t, 2.0f/power) * extents.x * Mathf.Sign(norm.x),
+			Mathf.Pow(sin_t, 2.0f/power) * extents.y * Mathf.Sign(norm.y)
+		);
+	}
+
+	Vector2 projectToExtents(Vector2 norm, Vector2 extents) {
+		if (Mathf.Approximately(norm.x, 0)) {
+			return Vector2.up * extents.y * Mathf.Sign(norm.y);
+		} else {
+			if (Mathf.Abs(norm.y) < Mathf.Abs(norm.x) * extents.y / extents.x) {
+				return new Vector2(
+					extents.x * Mathf.Sign(norm.x),
+					norm.y * extents.x / Mathf.Abs(norm.x)
+				);
+			} else {
+				return new Vector2(
+					norm.x * extents.y / Mathf.Abs(norm.y),
+					extents.y * Mathf.Sign(norm.y)
+				);
+			}
+		}
 	}
 
 	void OnDrawGizmosSelected() {
